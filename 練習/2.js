011b3253ae5e677b226b0c70f124287068d371a2
liu99002 <liu99002@gmail.com>
2023-04-21 11:34:54 +0800

    1.html

--- 練習/2.js
+++ 練習/2.js
@@ -1,2 +1,17 @@
-var list = ['a', 'dog', 'chase', 'a', 'cat']
+function count(words){
+    var cmap = {}
+    for (let word of words){
+        let c = cmap[word]
+        if (c == null){
+            cmap[word] = 1
+        }
+        else{
+            cmap[word] = cmap[word] + 1
+        }
+    }
+    return cmap
+}
 
+let list = ["a", "dog", "chase", "a", "cat"]
+let cmap = count(list)
+console.log(cmap)
